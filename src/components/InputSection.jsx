d8b6a183bbdc33e745dd84e82287f234565368f8
thetramp22 <scottstarks.games@gmail.com>
2023-10-23 17:01:51 -0700

    Added styling for InputField and InputSection

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import InputField from "./InputField";
+import "../styles/InputSection.css";
 
 const InputSection = ({ name, fields }) => {
   return (
-    <div>
+    <div className="input-section">
       <h2>{name}</h2>
       {fields.map((field) => (
         <InputField
